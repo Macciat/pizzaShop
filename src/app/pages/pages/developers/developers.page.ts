966efb116cba7ca93ef40b32be27ed74994fe256
ShawnAgres <qsapagres@tip.edu.ph>
2025-11-08 02:35:43 +0800

    Delete src/app/pages/pages directory

--- src/app/pages/pages/developers/developers.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-developers',
-  templateUrl: './developers.page.html',
-  styleUrls: ['./developers.page.scss']
-})
-export class DevelopersPage {}
