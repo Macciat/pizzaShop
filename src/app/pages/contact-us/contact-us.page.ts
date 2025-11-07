89016fda6028faec29935f95aafafb60a2a8e40f
ShawnAgres <qsapagres@tip.edu.ph>
2025-11-08 02:36:23 +0800

    Add files via upload

--- src/app/pages/contact-us/contact-us.page.ts
+++ src/app/pages/contact-us/contact-us.page.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.page.html',
-  styleUrls: ['./contact-us.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  styleUrls: ['./contact-us.page.scss']
 })
-export class ContactUsPage implements OnInit {
+export class ContactUsPage {
 
-  constructor() { }
+  openGmail(event: Event) {
+    event.preventDefault();
 
-  ngOnInit() {
+    const firstName = (document.getElementById('firstName') as HTMLInputElement).value;
+    const lastName = (document.getElementById('lastName') as HTMLInputElement).value;
+    const concernType = (document.getElementById('concernType') as HTMLSelectElement).value;
+    const message = (document.getElementById('message') as HTMLTextAreaElement).value;
+
+    const recipient = 'qsapagres@tip.edu.ph';
+    const subject = encodeURIComponent(`[${concernType}] Message from ${firstName} ${lastName}`);
+    const body = encodeURIComponent(
+      `Name: ${firstName} ${lastName}\nType of Concern: ${concernType}\n\nMessage:\n${message}`
+    );
+
+    const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${recipient}&su=${subject}&body=${body}`;
+    window.open(gmailUrl, '_blank');
   }
-
 }
